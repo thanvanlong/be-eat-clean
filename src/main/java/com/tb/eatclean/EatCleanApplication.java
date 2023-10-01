5ad05d1c3df946b66ec7700cab32ccd7dedbc3b9
annh <longthan366@gmail.com>
2023-10-01 15:41:02 +0700

    init proj

---
+++ src/main/java/com/tb/eatclean/EatCleanApplication.java
@@ -0,0 +1,13 @@
+package com.tb.eatclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatCleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatCleanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tb/eatclean/EatCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tb.eatclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatCleanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
